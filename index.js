97fda3a60a992184785d3159713dd61ccca8e59a
小昆昆 <429121430@qq.com>
2023-08-02 15:44:25 +0800

    edit text

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages kun :)'
+  document.getElementById('main-content').innerHTML = 'Hello, 爱你的小昆昆 :)'
 }
